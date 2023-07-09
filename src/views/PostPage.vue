b59768fde2b4d26b21c76805f78204492ba8cc08
Youngtechie <olaegbeolamilekan1@gmail.com>
2023-07-09 19:33:38 +0100

    [comment out SSR]::

--- src/views/PostPage.vue
+++ src/views/PostPage.vue
@@ -473,4 +473,31 @@ h5 {
     text-align: center;
     align-self: center;
 }
+#warningShow {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    padding: 10px;
+    border: 1px outset #efefef;
+    display: none;
+    text-align: center;
+    height: 200px;
+    width: 200px;
+    border-radius: 10px;
+    font-weight: bolder;
+    align-items: center;
+    justify-content: center;
+}
+
+.DayApp #warningShow {
+    color: #efefef;
+    background-color: black;
+}
+
+.NightApp #warningShow {
+    color: black;
+    background-color: #efefef;
+}
+
 </style>
